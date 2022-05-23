2a02db382f564c7cec161713c99685d17ee2b651
Joel S <85464337+Joel-Strand@users.noreply.github.com>
2022-05-23 12:58:33 -0400

    Basic Hashing for the blocks

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express')
 const path = require('path')
+const { createHash } = require('node:crypto')
 
 const app = express()
 const port = process.env.PORT || 8080;
+const hash = createHash('sha256')
 
-let blockchain = [{
-        name: 'First Block'
+
+var blockchain = [{
+    'data': {
+        'name': 'Joel Strand',
+        'age': 13,
+        'grade': 11,
+        'time': 12,
     },
-    {
-        name: 'Second Block'
-    }
-];
+    'proof': 100,
+    'hash': hash.update('data' + 'proof').copy().digest('hex')
+}]
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/block', (req, res) => {
-
-    return res.json(blockchain);
-
-});
+app.get('/api/block', (req, res) => { return res.json(blockchain); });
 
 app.get('/api/block/:id', (req, res) => {
 
@@ -26,17 +28,8 @@ app.get('/api/block/:id', (req, res) => {
     if (blockchain[req.params.id] == undefined) return res.status(404).send("Block not found!");
 
     res.json(blockchain[req.params.id]);
-
 });
 
-app.get('/api/block/:id/delete', (req, res) => {
+app.get('/api/block/:id/delete', (req, res) => { res.json(blockchain.splice(req.params.id, 1)[0]) })
 
-    res.json(blockchain.splice(req.params.id, 1)[0]);
-
-})
-
-app.listen(port, () => {
-
-    console.log('Listening on port ' + port)
-
-})
+app.listen(port, () => { console.log('Listening on port ' + port) })
