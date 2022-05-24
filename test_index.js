9ed41b010c249b24c394938a0be3a8ad562b4583
Joel S <85464337+Joel-Strand@users.noreply.github.com>
2022-05-24 13:33:44 -0400

    Site Updated.
    Blockchain implementation still in the works.

--- test_index.js
+++ test_index.js
@@ -1,54 +1,20 @@
+// Libraries 
 const express = require('express')
 const path = require('path')
 const { createHash } = require('node:crypto')
-
 const app = express()
 const port = process.env.PORT || 8080;
 const hash = createHash('sha256')
 
-let blockchain = [];
+// Blockchain Initialization
+var Blockchain = require('./blockchain.js')
+let blockchain = new Blockchain();
 
-function hashFunc(data, prevHash) {
 
-    let hashData = "";
-
-    while (!hashData.endsWith("0000")) {
-
-        hashData = hash.update(data.toString() + prevHash.toString()).copy().digest('hex');
-
-    }
-
-    return hashData;
-
-}
-
-function pushBlock(block) {
-
-    let _block = {
-        data: block || {},
-        hash: hashFunc(JSON.stringify(block), blockchain.slice(-1).hash || "") || ""
-    };
-
-    blockchain.push(_block);
-
-    return _block;
-
-}
-
+// App Initialization
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/blockchain', (req, res) => { return res.json(blockchain); });
-
-app.get('/api/block/add', (req, res) => {
-
-    res.json(
-        pushBlock({
-            title: req.query.title,
-            description: req.query.description
-        })
-    );
-
-});
 
 app.get('/api/block/:id', (req, res) => {
 
@@ -56,9 +22,10 @@ app.get('/api/block/:id', (req, res) => {
     if (blockchain[req.params.id] == undefined) return res.status(404).send("Block not found!");
 
     res.json(blockchain[req.params.id]);
-
 });
 
 app.get('/api/block/:id/delete', (req, res) => { res.json(blockchain.splice(req.params.id, 1)[0]) })
+
+// app.get('/api/mine')
 
 app.listen(port, () => { console.log('Listening on port ' + port) })
